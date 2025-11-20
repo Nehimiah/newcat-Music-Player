9551b15f39466b15073bce9101463b04d6682918
Nehy <74353375+Nehimiah@users.noreply.github.com>
2025-11-19 22:13:48 -0800

    Update firebase.ts

--- web/lib/firebase.ts
+++ web/lib/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAczHDNVAyrfZHCSTp_O3m60jUi_2q4Er0",
+  authDomain: "newcat-d02c0.firebaseapp.com",
+  projectId: "newcat-d02c0",
+  storageBucket: "newcat-d02c0.firebasestorage.app",
+  messagingSenderId: "980860666498",
+  appId: "1:980860666498:web:1f97e3951c3acbf17c6a9b",
+  measurementId: "G-60HWMH9VM7"
 };
 
 export default firebaseConfig;
